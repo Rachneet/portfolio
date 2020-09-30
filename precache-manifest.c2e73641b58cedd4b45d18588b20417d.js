9dce19ee230d90901d45c0995ebfe7657ab8ab56
Rachneet Singh Sachdeva <rachneetsachdeva@yahoo.com>
2020-09-30 13:24:17 +0200

    Updates

---
+++ precache-manifest.c2e73641b58cedd4b45d18588b20417d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ffdb5d0bb42f770938c92ff56ae6227",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a2e2105cab47d0b0354f",
+    "url": "/portfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/portfolio/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/portfolio/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2e2105cab47d0b0354f",
+    "url": "/portfolio/static/js/main.dab72791.chunk.js"
+  },
+  {
+    "revision": "d6ca8614ab4df5b1bf68",
+    "url": "/portfolio/static/js/runtime-main.44645aae.js"
+  }
+]);
